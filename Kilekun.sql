2258c77407f0afe5eda48a20a5bb4497d96c4738
Kilekun <kilekunonajide@gmail.com>
2025-01-17 08:22:13 +0100

    Add files via upload
    First SQL assignment

---
+++ Kilekun.sql
@@ -0,0 +1,17 @@
+CREATE TABLE Students (
+    ID INT,
+    Name VARCHAR(50),
+    MatriculationNumber VARCHAR(20),
+    Gender VARCHAR(7) 
+);
+INSERT INTO Students (ID, Name, MatriculationNumber, Gender) 
+VALUES (200345, 'david ezekiel', '20CJ00034', 'Male'),
+       (210034, 'chris keets', '21CJ00232', 'Male'),
+       (210022, 'Hemsworth chris', '21CJ00433', 'Male'),
+       (210023, 'phillip bass', '21CJ00244', 'Male'),
+       (190011, 'rufus othello', '19CJ00135', 'Male'),
+       (200034, 'Bukayo Saka', '20CJ00564', 'Male'),
+       (200024, 'William minerva', '20CJ00165', 'Male'),
+       (210064, 'Patricia dent', '21CJ00158', 'Male'),
+       (200025, 'Thomas rees', '20CJ00567', 'Female'),
+       (190035, 'Olivia Rodri', '19CJ00354', 'Female');
